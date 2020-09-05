f02c70fd278388c545964f70fb7cd3d94285e7fd
Sam Nishita <saminator21@gmail.com>
2020-09-04 21:39:07 -0700

    shot diagram and other nodes will resize based on the dimesions of the window

--- src/main/java/controllers/SimpleController.java
+++ src/main/java/controllers/SimpleController.java
@@ -5,10 +5,71 @@
  */
 package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  *
  * @author samnishita
  */
-public class SimpleController {
+public class SimpleController implements Initializable {
+
+    @FXML
+    ImageView imageview;
+    @FXML
+    BorderPane borderpane;
+//    @FXML
+//    Pane pane;
+//    @FXML
+//    Button buttonspace;
+//    @FXML
+//    ImageView ivspace;
+    @FXML
+    GridPane gridpane;
+    @FXML
+    Label label;
+    @FXML
+    VBox vbox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    public BorderPane getBP() {
+        return this.borderpane;
+    }
+
+    public ImageView getIV(){
+        return this.imageview;
+    }
     
+//    public Pane getPane(){
+//        return this.pane;
+//    }
+//    public Button getButtonSpace(){
+//        return this.buttonspace;
+//    }
+//    public ImageView getIVSpace(){
+//        return this.ivspace;
+//    }
+    public GridPane getGridPane(){
+        return this.gridpane;
+    }
+    public Label getLabel(){
+        return this.label;
+    }
+    public VBox getVBox(){
+        return this.vbox;
+    }
 }
--- src/main/java/mainapp/Main.java
+++ src/main/java/mainapp/Main.java
@@ -5,10 +5,18 @@
  */
 package mainapp;
 
+import controllers.SimpleController;
+import java.awt.Button;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -17,13 +25,54 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
+    private FXMLLoader loader;
+    private SimpleController sc;
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = (Parent) FXMLLoader.load(getClass().getResource("/fxml/simple.fxml"));
+//        loader = new FXMLLoader(getClass().getResource("/fxml/simple.fxml"));
+//        Parent root = (Parent) loader.load();
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.setTitle("Dynamic NBA Shot Charts");
+//        stage.setMinHeight(650);
+//        stage.setMinWidth(800);
+//        stage.show();
+//        sc = loader.getController();
+//        BorderPane bp = sc.getBP();
+//        System.out.println(bp + ": " + bp.getWidth() + ", " + bp.getHeight());
+//        ImageView iv = sc.getIV();
+//        System.out.println(iv + ": " + iv.getFitWidth() + ", " + iv.getFitHeight());
+//        iv.setFitHeight(1776);
+//        iv.setFitWidth(1890);
+//        iv.fitHeightProperty().bind(bp.heightProperty().divide(1.6));
+//        iv.setPreserveRatio(true);
+//        ImageView ivspace = sc.getIVSpace();
+//        ivspace.setFitWidth(600);
+//        ivspace.fitWidthProperty().bind(bp.widthProperty().multiply(2));
+//        ivspace.setPreserveRatio(true);
+
+        loader = new FXMLLoader(getClass().getResource("/fxml/simplegrid.fxml"));
+        Parent root = (Parent) loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Dynamic NBA Shot Charts");
+        stage.setMinHeight(650);
+        stage.setMinWidth(900);
+        
         stage.show();
+        sc = loader.getController();
+        GridPane gp = sc.getGridPane();
+        VBox vbox = sc.getVBox();
+        System.out.println(vbox);
+        vbox.prefWidthProperty().bind(scene.widthProperty());
+        vbox.prefHeightProperty().bind(scene.heightProperty());
+        ImageView iv = sc.getIV();
+        iv.setFitHeight(1776);
+        iv.setFitWidth(1890);
+        iv.fitWidthProperty().bind(gp.widthProperty().divide(1.75));
+        iv.fitHeightProperty().bind(gp.heightProperty().divide(1.2));
+        iv.setPreserveRatio(true);
     }
 
     public static void main(String[] args) {
